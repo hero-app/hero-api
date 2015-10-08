5d596a9659439d97f68af25579ffacbb20559db3
Shahal Meril <shahal.meril@safebreach.com>
2015-10-08 16:23:40 +0300

    Add landing page

--- api/index.js
+++ api/index.js
@@ -7,6 +7,9 @@ var express = require('express');
 
 var router = express.Router();
 
+router.get('/', function(req, res) {
+    res.send('hello');
+});
 router.use('/login', require('./login'));
 
 
