dfc4bd5b9ff9ad009b9699c50177b9d6e94aa6d2
Shahal Meril <shahal.meril@safebreach.com>
2015-10-08 16:48:56 +0300

    Stuff

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(function(req, res, next) {
 });
 
 
-app.listen(3000, function() {
-    console.log('*** Hero is listening on port 3000 ***');
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+    console.log('*** Hero is listening on port', port, '***');
 });
